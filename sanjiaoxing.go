1af915d8d7dec38034718327d9b98f6aceeaa905
Nextlin <448549012@qq.com>
2021-05-04 01:27:19 +0800

    这是提交go代码的测试的开始

---
+++ sanjiaoxing.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 10; i >= 0; i-- {
+		for l := 0; l < 10-i; l++ {
+			fmt.Print(" ")
+		}
+		for j := 0; j <= i; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
+}
